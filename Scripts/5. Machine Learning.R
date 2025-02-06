77009c3fd5ffd2c027c29fe4322f60fffeb4be66
Umar Faruk Saidu <67103397+UFSaidu@users.noreply.github.com>
2025-02-06 07:51:56 +0100

    Relative expression of hub genes

---
+++ Scripts/5. Machine Learning.R
@@ -0,0 +1,56 @@
+
+title: "Machine Learning"
+author: "Umar Faruk Saidu"
+date: "2025-01-05"
+
+#======Expression difference of the hub genes in Training data======#
+
+# Subset the expression data and meta data to include only the 16 hub genes
+gene_list <- true_hub$Gene
+hub_genes_exp <- filtered_data[gene_list, ]
+hub_genes_meta <- meta_data
+hub_genes_exp <- as.data.frame(t(hub_genes_exp))
+hub_genes_exp$Group <- hub_genes_meta$Group
+
+# Reshape the data for ggplot2
+hub_genes_melt <- melt(hub_genes_exp,
+                       id.vars = "Group",
+                       variable.name = "Gene",
+                       value.name = "Expression")
+
+# Create boxplot for each gene
+plot_list <- list()
+
+hub_genes_exp$Group <- as.factor(hub_genes_exp$Group)
+comparisons <- list(c("HC", "TBI"))
+
+for (gene in unique(hub_genes_melt$Gene)) {
+  gene_data <- subset(hub_genes_melt, Gene == gene)
+  
+  p <- ggplot(gene_data, 
+              aes(x = Group, y = Expression, fill = Group)) +
+    geom_boxplot(outlier.shape = NA, alpha = 0.7) +
+    geom_jitter(width = 0.2, size = 1, alpha = 0.6) +
+    scale_fill_manual(values = c("TBI" = "red", "HC" = "blue")) +
+    stat_compare_means(comparisons = comparisons, method = "t.test", 
+                       label = "p.signif") +
+    theme_minimal() +
+    labs(title = paste(gene),
+         x = "Group", y = "Expression") +
+    theme(plot.title = element_text(hjust = 0.5, face = "bold"),
+          plot.margin = margin(t = 20),
+          legend.position = "none",
+          panel.border = element_blank(),
+          axis.line = element_line(color = "black"))
+  
+  plot_list[[gene]] <- p
+    
+}
+
+# Use grid extra to display the plots
+combined_plot <- do.call(grid.arrange, c(plot_list, ncol = 4))
+
+ggsave("~/WGCNALASSO/Output/hub_genes_box_plot.png", 
+       combined_plot, width = 26, 
+       height = 15, background = "white", dpi = 600)
+
